ac977a1495830ef6111ce53e6c004f875aa7d539
Bryan Han <bryanhan0612@gmail.com>
2026-04-29 20:04:31 -0400

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #if the length of s and t are not the same, then they cannot be anagrams of each other
+        #use hashmap to count the amount of each letter, if the hashmaps are equal to eachother then return true otherwise false
+        if len(s) != len(t):
+            return False
+        
+        s_count = {}
+        t_count = {}
+
+        for c in s:
+            if c in s_count:
+                s_count[c] += 1
+            else:
+                s_count[c] = 1 
+
+        for c in t:
+            if c in t_count:
+                t_count[c] += 1
+            else:
+                t_count[c] = 1 
+
+        return s_count == t_count
+        
+        
+
+
+        
+
+        
+        
+        
+        
