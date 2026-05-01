1c6eafb75b0ecdadf82dac015a758b817d85f735
Bryan Han <bryanhan0612@gmail.com>
2026-05-01 13:31:45 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closed = {'}' : '{', ']' : '[', ')' : '('}
+
+        for char in s: 
+            if char in closed:
+                if stack and stack[-1] == closed[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        
+        if stack:
+            return False
+        else:
+            return True
+        
